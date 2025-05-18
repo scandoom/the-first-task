8fdca8102e8146ba475c72bd131c875811749bfb
Scandoom <wiwerin@yandex.ru>
2025-05-18 17:14:13 +0300

    first commit

--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,7 +1,14 @@
 package daysteps
 
 import (
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/Yandex-Practicum/tracker/internal/spentcalories"
 )
 
 const (
@@ -12,9 +19,49 @@ const (
 )
 
 func parsePackage(data string) (int, time.Duration, error) {
-	// TODO: реализовать функцию
+	value := strings.Split(data, ",")
+	if len(value) != 2 {
+		return 0, 0, errors.New("!= 2")
+	}
+
+	valueStep, err := strconv.Atoi(value[0])
+	if err != nil {
+		return 0, 0, err
+	}
+	if valueStep <= 0 {
+		return 0, 0, errors.New("0 шагов")
+	}
+
+	valueDuration, err := time.ParseDuration(value[1])
+	if err != nil {
+		return 0, 0, err
+	}
+	if valueDuration <= 0 {
+		return 0, 0, errors.New("0 часов")
+	}
+
+	return valueStep, valueDuration, nil
 }
 
 func DayActionInfo(data string, weight, height float64) string {
-	// TODO: реализовать функцию
+	valueStep, valueDuration, err := parsePackage(data)
+	if err != nil {
+		log.Printf("Ошибка парсинга: %v", err)
+		return ""
+	}
+	if valueStep <= 0 {
+		return ""
+	}
+	if valueDuration <= 0 {
+		return ""
+	}
+
+	distance := (float64(valueStep) * stepLength) / mInKm
+	callories, err := spentcalories.WalkingSpentCalories(valueStep, weight, height, valueDuration)
+	if err != nil {
+		return ""
+	}
+
+	return fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\nВы сожгли %.2f ккал.\n", valueStep, distance, callories)
+
 }
